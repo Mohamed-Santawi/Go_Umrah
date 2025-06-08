aa9ccd317702e670e1f7e6261d3decad2cc67677
Mohamed Santawi <mohamedsant3@gmail.com>
2025-06-08 22:18:08 +0300

    first

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJtHZhCpjw2makXm26PddrSZfie_IVuDk",
-  authDomain: "game-recharge-app.firebaseapp.com",
-  projectId: "game-recharge-app",
-  storageBucket: "game-recharge-app.firebasestorage.app",
-  messagingSenderId: "891201362263",
-  appId: "1:891201362263:web:8d2c98a35da0bbd96e4612",
-  measurementId: "G-DX8ESK6LXY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
